cfbdd0345d99bad382f792d753a9c4c915d8a464
aishakhan267 <49521671+aishakhan267@users.noreply.github.com>
2019-07-11 17:33:13 +0500

    whtsapp data uploaded

---
+++ script.js
@@ -0,0 +1,141 @@
+let circleOrganization = {
+    name:"Tech karo",
+    purpose:"Women Empowerment",
+    passedCohort:1,
+    isProgress:"true",
+    courseTitle:"Web Development",
+    currentStrength:100
+};
+
+let currentStrength = [
+    {
+        name:"Aisha",
+        contactNumber:"0333-8009665",
+        education:"Bachelor of Science",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:["Drawing", "Coding"]
+    },
+    {
+        name:"Sameer",
+        contactNumber:"0333-5007889",
+        education:"F.Sc",
+        hasPromoCode:true,
+        group:"A",
+        hobbies:["Riding", "Singing"]
+    },
+    {
+        name:"Saim",
+        contactNumber:"0300-5092789",
+        education:"F.Sc",
+        hasPromoCode:false,
+        group:"A",
+        hobbies:["Riding", "Painting", "walking"]
+    },
+    {
+        name:"Amertiya",
+        contactNumber:"0345-5007000",
+        education:"M.Sc",
+        hasPromoCode:true,
+        group:"A",
+        hobbies:["Cooking", "Singing"]
+    },
+    {
+        name:"Armia",
+        contactNumber:"0333-0908765",
+        education:"B.Sc",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:"Coding"
+    },
+    {
+        name:"Rubab",
+        contactNumber:"0333-7898900",
+        education:"Matriculation",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:"Coding"
+    },
+    {
+        name:"Maha",
+        contactNumber:"0343-5908705",
+        education:"B.Sc",
+        hasPromoCode:false,
+        group:"B",
+        hobbies:["Gardening", "Coding", "Cooking"]
+    },
+    {
+        name:"Sadia",
+        contactNumber:"0346-1908995",
+        education:"M.A",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:"Coding"
+    },
+    {
+        name:"Lubna",
+        contactNumber:"0300-0901769",
+        education:"B.Sc",
+        hasPromoCode:true,
+        group:"A",
+        hobbies:["Poerty", "Reading"]
+    },
+    {
+        name:"Sarfaraz",
+        contactNumber:"0343-0907775",
+        education:"M.Phill",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:["Riding", "Fishing", "Drafting"]
+    }
+];
+let whatsApp = [
+    {
+        name:"Aisha Khan",
+        contactNumber:"0333-8009665",
+        profilePicture:"https://i.ibb.co/kDJSLnb/doll-jpeg.jpg",
+        readMessage:1,
+        lastMessage:"will meet in class",
+        status:"Seen"
+    },
+    {
+        name:"Ammara",
+        contactNumber:"0345-8669665",
+        profilePicture:"https://i.ibb.co/q0pg7TP/doll2-jpeg.jpg",
+        readMessage:0,
+        lastMessage:"I am coming to Karachi",
+        status:"Seen"  
+    },
+    {
+        name:"Saleh",
+        contactNumber:"0300-8001005",
+        profilePicture:"https://i.ibb.co/MMXgHTG/flower-jpeg.jpg",
+        readMessage:12,
+        lastMessage:"I have selected for Tech Karo course",
+        status:"Unseen"
+    },
+    {
+        name:"Alishbah",
+        contactNumber:"0346-2334965",
+        profilePicture:"https://i.ibb.co/fv02Jw1/ship-jpeg.jpg",
+        readMessage:"All",
+        lastMessage:"Pay my regards to all",
+        status:"Seen"
+    },
+    {
+        name:"Alina",
+        contactNumber:"0300-6765431",
+        profilePicture:"https://i.ibb.co/pvmWFSY/horse-jpeg.jpg",
+        readMessage:"All",
+        lastMessage:"Good Night",
+        status:"Seen"
+    },
+    {
+        name:"Danish",
+        contactNumber:"0346-8009860",
+        profilePicture:"https://i.ibb.co/jDpc5FG/mobile-jpeg.jpg",
+        readMessage:2,
+        lastMessage:"Barish horhe hy hlki hlki",
+        status:"Seen"
+    }
+];
